3f23d5ade4ae9de068660e0c6b43a080ef9d699e
Abhiram Sathasivam <abhiram.satha@gmail.com>
2021-07-07 23:31:45 +0000

    W2D2 Ruby & Sinatra 1

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,15 @@
-get '/' do 
-    File.read(File.join('app/views', 'index.html'))
+#get '/' do 
+ #   File.read(File.join('app/views', 'index.html'))
+# end
+
+get '/' do
+    username = "sharky_j"
+    avatar_url = "http://naserca.com/images/sharky_j.jpg"
+    photo_url = "http://naserca.com/images/shark.jpg"
+    time_ago_in_minutes = 15
+    like_count = 0
+    comment_count = 1
+    comments = [
+      "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
+    ]
 end
