01cb2394e7e3d459f4cf22ae8113d48d396f248a
Abhiram Sathasivam <abhiram.satha@gmail.com>
2021-07-07 23:54:26 +0000

    W2D2 Ruby Sinatra 2

--- app/actions.rb
+++ app/actions.rb
@@ -1,15 +1,21 @@
-#get '/' do 
- #   File.read(File.join('app/views', 'index.html'))
-# end
-
-get '/' do
+def humanized_time_ago(time_ago_in_minutes)
+    if time_ago_in_minutes >= 60
+      "#{time_ago_in_minutes / 60} hours ago"
+    else
+      "#{time_ago_in_minutes} minutes ago"
+    end
+  end
+  
+  get '/' do
     username = "sharky_j"
     avatar_url = "http://naserca.com/images/sharky_j.jpg"
     photo_url = "http://naserca.com/images/shark.jpg"
-    time_ago_in_minutes = 15
+    time_ago_in_minutes = 115
     like_count = 0
     comment_count = 1
     comments = [
       "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
     ]
-end
+  
+    humanized_time_ago(time_ago_in_minutes)
+  end
